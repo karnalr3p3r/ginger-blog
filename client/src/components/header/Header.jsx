5eafba4caa7b6a93689fefde97ed17eb3922740b
karnalr3p3r <devereauxma97@gmail.com>
2022-08-12 01:02:40 -0500

    changed some things

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -8,8 +8,7 @@ export default function Header() {
       </div>
       <img
         className="headerImg"
-        src="https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_2044,w_3635,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1602988205/Library_iwde6f"
-        alt=""
+        src="https://cdn.britannica.com/05/99605-050-7C2A6766/view-Ireland-coastline.jpg"
       />
     </div>
   );
