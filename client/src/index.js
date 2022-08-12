c80bf3d9c3ad511453cce433d52744770e899f24
karnalr3p3r <devereauxma97@gmail.com>
2022-08-12 02:58:14 -0500

    mmm

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { ContextProvider } from "./context/Context";
-import "./styles/main.scss";
 
 ReactDOM.render(
   <React.StrictMode>
