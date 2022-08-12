38c27d270ce8784268e193503308fbda53143e9b
karnalr3p3r <devereauxma97@gmail.com>
2022-08-12 06:05:35 -0500

    my first commit

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
         </Route>
         <Route path="/register">{user ? <Home /> : <Register />}</Route>
         <Route path="/login">{user ? <Home /> : <Login />}</Route>
-        <Route path="/write">{user ? <Write /> : <Login />}</Route>
+        <Route path="/write">{user ? <Write /> : <Write />}</Route>
         <Route path="/settings">{user ? <Settings /> : <Login />}</Route>
         <Route path="/post/:postId">
           <Single />
