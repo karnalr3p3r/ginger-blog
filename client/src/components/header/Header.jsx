c2f5549763d885c599a20875b6c65faa2e3fdd6f
karnalr3p3r <devereauxma97@gmail.com>
2022-08-12 03:11:35 -0500

    mmm

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -7,11 +7,13 @@ export default function Header() {
         <h1 className="headerTitleLg">The Life of a Ginger</h1>
         <h3 className="headerQuestion">Can You Find Waldo?</h3>
       </div>
-      <img
-        className="headerImg"
-        src="http://www.paperflite.com/sites/default/files/2020-01/WaldoBanner.jpg"
-        alt=""
-      />
+      <div className="headerImgContainer">
+        <img
+          className="headerImg"
+          src="http://www.paperflite.com/sites/default/files/2020-01/WaldoBanner.jpg"
+          alt=""
+        />
+      </div>
     </div>
   );
 }
